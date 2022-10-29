d57d894468b9e04838e7e5a968eade9628bf0076
kuvii <kuvilet@gmail.com>
2022-10-29 18:38:22 +0100

    Style(App): Cambiar fondo y estructura

--- App.js
+++ App.js
@@ -20,14 +20,14 @@ export default function App() {
     setProducts( () => [...products, product] )
   }
 
-
   return (
     <View style={styles.container}>
+      <View style={styles.productInput}>
         <ProductInput
           product= {product}
           setProduct={setProduct}
           onProductAdd = { addProductHandler }
-        />
+          />
         <View style={styles.productList}>
           <ScrollView>
             { 
@@ -36,6 +36,7 @@ export default function App() {
                 : products.map((product, idx) =>  <ListItem key={idx + product.name} product={product}  />)
               }
             </ScrollView>
+              </View>
         </View>
     </View>
   )
@@ -43,15 +44,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F3D031',
     alignItems: 'center',
-    justifyContent: 'flex-start',
   },
   productList: {
     flex: 4,
     margin: 20,
     width: '80%',
+  },
+  productInput: {
+    marginTop: 50,
+    width: "90%",
+    alignItems: 'center'
   }
 });
